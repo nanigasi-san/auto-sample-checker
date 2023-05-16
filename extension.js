506e165fe878fbc3fd91edf410c52cfd5d048132
nanigasi_san <nanigasi.py@gmail.com>
2023-05-16 21:16:01 +0900

    submit以外を整えた

--- extension.js
+++ extension.js
@@ -2,24 +2,32 @@ const vscode = require('vscode');
 const { exec } = require('child_process');
 const path = require('path');
 
-let url = '';
 
 function activate(context) {
 	// 拡張が有効になったときに実行されるコード
 
 	// "register problem" ボタンを作成し、クリックイベントを設定する
 	const registerProblemButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-	registerProblemButton.text = '$(rocket) Register Problem';
+	registerProblemButton.text = '$(cloud-download) Register Problem';
 	registerProblemButton.command = 'extension.registerProblem';
 	registerProblemButton.show();
 	
 	// "test sample case" ボタンを作成し、クリックイベントを設定する
 	const testSampleCaseButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-	testSampleCaseButton.text = '$(play) Test Sample Case';
+	testSampleCaseButton.text = '$(watch) Test Sample Case';
 	testSampleCaseButton.command = 'extension.testSampleCase';
+
+	const submitCodeButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+	submitCodeButton.text = '$(rocket) Submit Code';
+	submitCodeButton.command = 'extension.submitCode';
 
 	// "register problem" ボタンがクリックされたときの処理
 	context.subscriptions.push(vscode.commands.registerCommand('extension.registerProblem', () => {
+		let url = '';
+		let downloaded = false;
+		let passed = false;
+		testSampleCaseButton.hide();
+		submitCodeButton.hide();
 		// テキストボックスと submit ボタンを表示する
 		vscode.window.showInputBox({ prompt: 'Enter the URL:' }).then((input) => {
 			if (input) {
@@ -32,10 +40,9 @@ function activate(context) {
 		});
 	}));
 
-
-
 	// "test sample case" ボタンがクリックされたときの処理
 	context.subscriptions.push(vscode.commands.registerCommand('extension.testSampleCase', () => {
+		submitCodeButton.hide();
 		console.log('pushed test');
 		const current_file = vscode.window.activeTextEditor.document.uri.fsPath;
 		console.log(`now file: ${current_file}`);
@@ -50,6 +57,7 @@ function activate(context) {
 				console.error(`Error executing command: ${command}`);
 				console.log(`[stdout]\n${stdout}\n-------------`);
 				console.error(`[stderr]\n${stderr}\n-------------`);
+
 				if (stdout.includes('test failed')) {
 					vscode.window.showErrorMessage("Fail sample cases");
 				} else {
@@ -59,6 +67,7 @@ function activate(context) {
 			console.log(stdout, stderr);
 			if (stdout.includes('test success')) {
 				vscode.window.showInformationMessage("Pass sample cases");
+				submitCodeButton.show();
 			}
 		});
 	}
